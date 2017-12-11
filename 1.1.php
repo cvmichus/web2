c971a03f3a1a34bd8a83dd49ec270cc1882688e2
cvmichus <c.michaus@incapital.mx>
2017-12-11 01:08:19 -0600

    web 2
    contenido en ajax

---
+++ 1.1.php
@@ -0,0 +1,116 @@
+                <div class="col-3">
+                    <div class="section-heading">
+                    
+                        <h1 class="section-title">01.ZEE</h1>
+                        
+                    </div>
+
+                </div>
+
+                <div class="col-2-3">
+
+                    <!--Icon Block-->
+                    <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.1s">
+                        <!--Icon-->
+                       
+                        <!--Icon Block Description-->
+                        <div class="icon-block-description">
+                            <h4>¿Qué es una ZEE? </h4>
+                       
+                        </div>
+                    </div>
+                    <!--End of Icon Block-->
+
+                    <!--Icon Block-->
+                    <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.3s">
+                        <!--Icon-->
+                       
+                        <!--Icon Block Description-->
+                        <div class="icon-block-description">
+                            
+                            <p>Las Zonas Económicas Especiales son áreas prioritarias del desarrollo nacional y el Estado promoverá las condiciones e incentivos para que, con la participación del sector privado y social, se contribuya al desarrollo económico y social de las regiones en las que se ubiquen, a través de una política industrial sustentable con vertientes sectoriales y regionales.</p>
+                        </div>
+                    </div>
+                    <!--End of Icon Block-->
+
+                    <!--Icon Block-->
+                    <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.5s">
+                        <!--Icon-->
+                        
+                        <!--Icon Block Description-->
+                        <div class="icon-block-description">
+                            <h4>¿Cuáles son los objetivos? </h4>
+                         
+                        </div>
+                    </div>
+                    <!--End of Icon Block-->
+
+                    <!--Icon Block-->
+                    <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.5s">
+                        <!--Icon-->
+                       
+                        <!--Icon Block Description-->
+                        <div class="icon-block-description">
+                           
+                            <p>Impulsar el crecimiento económico sostenible que, entre otros fines, reduzca la pobreza, permita la provisión de servicios básicos y expanda las oportunidades para vidas saludables y productivas, en las regiones del país que tengan mayores rezagos en desarrollo social, a través del fomento de la inversión, la productividad, la competitividad, el empleo y una mejor distribución del ingreso entre la población.</p>
+
+                            <p>
+                               Crear nuevos polos de desarrollo industrial en los estados más pobres del país, con objeto de inducir en éstos un cambio estructural hacia actividades económicas de mayor productividad.
+                            </p>
+
+                            <p>
+                            Ser la plataforma para incursionar nuevos mercados en Centroamérica, el Caribe, la
+Región Andina y el Pacífico, el sur-sureste tiene acceso a insumos y factores de producción más competitivos.
+                            </p>
+
+
+                        </div>
+                    </div>
+
+                     <!--Icon Block-->
+                    <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.1s">
+                        <!--Icon-->
+                       
+                        <!--Icon Block Description-->
+                        <div class="icon-block-description">
+                            <h4>¿Dónde se ubican las ZEE? </h4>
+                       
+                        </div>
+                    </div>
+                    <!--End of Icon Block-->
+
+                    <!--Icon Block-->
+                    <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.3s">
+                        <!--Icon-->
+                       
+                        <!--Icon Block Description-->
+                        <div class="icon-block-description">
+                            
+                            <p>
+                                Deberán ubicarse en las entidades federativas que, a la fecha de la emisión del Dictamen, se encuentren entre las diez entidades federativas con mayor incidencia de pobreza extrema, de acuerdo con la información oficial del Consejo Nacional de Evaluación de la Política de Desarrollo Social. 
+</p>
+<p>
+Deberán establecerse en áreas geográficas que representen una ubicación estratégica para el desarrollo de la actividad productiva, debido a la facilidad de integración con carreteras, aeropuertos, ferrocarriles, puertos o corredores interoceánicos y potencial de conectividad hacia otros mercados nacionales o internacionales. 
+</p>
+<p>
+Deberán prever la instalación de sectores productivos de acuerdo con las ventajas comparativas y vocación productiva presente o potencial de la Zona.
+</p>
+<p>
+Deberán establecerse en uno o más municipios cuya población conjunta, a la fecha de la emisión del Dictamen, sea entre 50 mil y 500 mil habitantes.
+
+                            </p>
+                        </div>
+                    </div>
+                    <!--End of Icon Block-->
+
+                    <!--Icon Block-->
+                    <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.5s">
+                        <!--Icon-->
+                        
+                        
+                    </div>
+                    <!--End of Icon Block-->
+                    <!--End of Icon Block-->
+                       
+
+                </div>
---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+
+    <!--Page Title-->
+    <title>OTYDUZEE</title>
+
+    <!--Meta Keywords and Description-->
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+
+    <!--Favicon-->
+    <link rel="shortcut icon" href="images/favicon.ico" title="Favicon"/>
+
+    <!-- Main CSS Files -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Namari Color CSS -->
+    <link rel="stylesheet" href="css/namari-color.css">
+
+    <!--Icon Fonts - Font Awesome Icons-->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- Animate CSS-->
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+
+    <!--Google Webfonts-->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+
+
+
+</head>
+<body onload="cargar('#contenido1', '1.1.php')" >
+
+<!-- Preloader -->
+<div id="preloader">
+    <div id="status" class="la-ball-triangle-path">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</div>
+<!--End of Preloader-->
+
+<div class="page-border" data-wow-duration="0.7s" data-wow-delay="0.2s">
+    <div class="top-border wow fadeInDown animated" style="visibility: visible; animation-name: fadeInDown;"></div>
+    <div class="right-border wow fadeInRight animated" style="visibility: visible; animation-name: fadeInRight;"></div>
+    <div class="bottom-border wow fadeInUp animated" style="visibility: visible; animation-name: fadeInUp;"></div>
+    <div class="left-border wow fadeInLeft animated" style="visibility: visible; animation-name: fadeInLeft;"></div>
+</div>
+
+<div id="wrapper">
+
+    <header id="banner" class="scrollto clearfix" data-enllax-ratio=".5">
+        <div id="header" class="nav-collapse">
+            <div class="row clearfix">
+                <div class="col-1">
+
+                    <!--Logo-->
+                    <div id="logo">
+
+                        <!--Logo that is shown on the banner-->
+                       
+                        <!--End of Banner Logo-->
+
+                        <!--The Logo that is shown on the sticky Navigation Bar-->
+                      
+                        <!--End of Navigation Logo-->
+
+                    </div>
+                    <!--End of Logo-->
+
+                  
+
+                    <!--Main Navigation-->
+                    <nav id="nav-main" style="font-family: Rokkitt; font-size: 10pt">
+                        <ul>
+                            <li>
+                                <a href="#banner">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">Antecendentes</a>
+                            </li>
+                            <li>
+                                <a href="#about">Zee y las instituciones</a>
+                            </li>
+                            <li>
+                                <a href="#services">Planeacion</a>
+                            </li>
+                            <li>
+                                <a href="#testimonials">Ciudades CES</a>
+                            </li>
+                            <li>
+                                <a href="#clients">Gobernanza</a>
+                            </li>
+                            <li>
+                                <a href="#pricing">Insumos</a>
+                            </li>
+                            <li>
+                                <a href="#pricing">Planes</a>
+                            </li>
+                            <li>
+                                <a href="#pricing">Glosario</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <!--End of Main Navigation-->
+
+                    <div id="nav-trigger"><span></span></div>
+                    <nav id="nav-mobile"></nav>
+
+                </div>
+            </div>
+        </div><!--End of Header-->
+
+    
+    </header>
+        
+   <main id="content"  id="banner-content" class="row clearfix">
+
+        <!--Introduction-->
+        <section id="about2" class="introduction scrollto">
+
+            <div class="row clearfix">
+
+                <div class="col-3">
+                    <div class="section-heading">
+                    
+                        
+                        
+                    </div>
+
+                </div>
+
+                <div class="col-2-3">
+
+                    <!--Icon Block-->
+                    <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.1s">
+                        <!--Icon-->
+                       
+                        <!--Icon Block Description-->
+                        <div class="icon-block-description">
+                            <h1 style="font-size: 4.5em; color:#e3313d;"><div style="border-style: outset; border-width: 4px; border-color: #e3313d;">OTYDUZEE</div> </h1>
+                            <br>
+                            <p><center style="font-size: 1em; font-family: Monserrat" >ORDENAMIENTO TERRITORIAL Y DESARROLLO <BR>URBANO  PARA ZONAS ECONOMICAS</center></p>
+                            <br><br>
+                       
+                        </div>
+                    </div>
+                    <!--End of Icon Block-->
+
+                    <!--Icon Block-->
+                    <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.3s">
+                        <!--Icon-->
+                       
+                        <!--Icon Block Description-->
+                        <div class="icon-block-description">
+                            
+                           
+                        </div>
+                    </div>
+                    <!--End of Icon Block-->
+
+                    <!--Icon Block-->
+                   
+                    <!--End of Icon Block-->
+
+                    <!--Icon Block-->
+                   
+
+                     <!--Icon Block-->
+                  
+                    <!--End of Icon Block-->
+
+                    <!--Icon Block-->
+                 
+                    <!--End of Icon Block-->
+
+                    <!--Icon Block-->
+                    <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.5s">
+                        <!--Icon-->
+                        
+                        
+                    </div>
+                    <!--End of Icon Block-->
+                    <!--End of Icon Block-->
+                       
+
+                </div>
+
+            </div>
+
+
+        </section>
+        <!--End of Introduction-->
+
+
+       
+
+    </main>
+    <!--End Main Content Area-->
+    
+    <!--Main Content Area-->
+    <main id="content">
+
+        <!--Introduction-->
+        <section id="about" class="introduction scrollto">
+
+            <div class="row clearfix" id="contenido1">
+
+
+
+            </div>
+                    
+                <div class="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.5s">
+                <!--Icon-->
+                <style>
+                    .ul2 {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #fff;
+
+}
+
+.li2 {
+    float: left;
+}
+
+.li2 a {
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 1em;
+    text-decoration: none;
+}
+
+.li2 a:hover {
+    color: #000;
+    font-weight: bold;
+    font-family: Monserrat;
+}
+
+                </style>
+
+                   <?php
+$secc = "h1";
+            ?>
+                        <ul class="ul2">
+                            <?php
+                                if ($secc == "h1") {
+                                    ?>
+                                          <li class="li2"><a href="#about" onclick="cargar('#contenido1', '1.1.php')" ><-</a></li>  
+                                    <?php
+                                }        
+                            ?>
+                      
+                        <li class="li2"><a href="#about" onclick="cargar('#contenido1', '1.2.php')" >1.2</a></li>
+                        <li class="li2"><a href="#about">1.3</a></li>
+                        <li class="li2"><a href="#about">1.4 </a></li>
+                        <li class="li2"><a href="#about">1.5</a></li>
+                        </ul>
+
+                </div>
+            
+        </section>
+
+        <script>
+function cargar(div, desde)
+{
+     $(div).load(desde);
+}
+</script>
+        <!--End of Introduction-->
+
+
+       
+
+    </main>
+    <!--End Main Content Area-->
+
+
+    <!--Footer-->
+    <footer id="landing-footer" class="clearfix">
+        <div class="row clearfix">
+
+            <p id="copyright" class="col-2">desarrollado por <a href="#">Canvas.mx</a></p>
+
+            <!--Social Icons in Footer-->
+            
+            </ul>
+            <!--End of Social Icons in Footer-->
+        </div>
+    </footer>
+    <!--End of Footer-->
+
+</div>
+
+<!-- Include JavaScript resources -->
+<script src="js/jquery.1.8.3.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/featherlight.min.js"></script>
+<script src="js/featherlight.gallery.min.js"></script>
+<script src="js/jquery.enllax.min.js"></script>
+<script src="js/jquery.scrollUp.min.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/jquery.stickyNavbar.min.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/images-loaded.min.js"></script>
+<script src="js/lightbox.min.js"></script>
+<script src="js/site.js"></script>
+
+
+</body>
+</html>

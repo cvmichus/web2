2de259defea47f9ce6a34ec836b8ade99e7a6b9f
carlos michaus <c.michaus@incapital.mx>
2017-12-19 17:28:35 -0600

    3.1 y 4.1

--- 2.2.php
+++ 2.2.php
@@ -2,9 +2,11 @@
                        
                         <div style="text-align:justify;">
                         
-<span style="font-size:12pt;font-weight:bold;color:#1e2f43;text-decoration:none;cursor: pointer;"><img src="img/icons/ciudadesyseticonospng-01.png" width="25%"><br>Lazaro Cardenas</span><br>
+<span style="font-size:12pt;font-weight:bold;color:#1e2f43;text-decoration:none;cursor: pointer;"><img src="img/icons/ciudadesyseticonospng-05.png" width="25%"><br>Tapachula</span><br>
 
-<span style="font-size:10pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="cargar('#contenido2', '2.2.php')" ><img src="img/icons/ciudadesyseticonospng-05.png" width="25%"><br>Tapachula</span><br>
+
+
+<span style="font-size:10pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="cargar('#contenido2', '2.1.php')" ><img src="img/icons/ciudadesyseticonospng-01.png" width="25%"><br>Lazaro Cardenas</span><br>
 
 
 
@@ -17,10 +19,10 @@
                         <div  style="text-align:justify;">
                         <ul style="list-style:none;cursor: pointer;color: #1e2f43;font-family:Montserrat;font-size:13.3px;
                         font weight:999;">
-                        <li onclick="ejecuta_ajax('2.2.1.php','','contenido1.2')" style="color:#e3313d;">La ciudad</li>
-                        <li onclick="ejecuta_ajax('2.2.2.php','','contenido1.2')" >Programa   municipal</li>
-                        <li href="#about" onclick="ejecuta_ajax('2.2.3.php','','contenido1.2')" >Indicadores</li>
-                        <li onclick="ejecuta_ajax('2.2.4.php','','contenido1.2')" >Ambito Espacial</li>
+                        <li onclick="ejecuta_ajax('2.2.5.php','','contenido1.2')" style="color:#e3313d;">La ciudad</li>
+                        <li onclick="ejecuta_ajax('2.2.6.php','','contenido1.2')" >Programa   municipal</li>
+                        <li href="#about" onclick="ejecuta_ajax('2.2.7.php','','contenido1.2')" >Indicadores</li>
+                        <li onclick="ejecuta_ajax('2.2.8.php','','contenido1.2')" >Ambito Espacial</li>
 
                         </ul>   
                         </div>
--- 4.1.php
+++ 4.1.php
@@ -2,9 +2,7 @@
                        
                         <div style="text-align:justify;">
                         
-<span style="font-size:12pt;font-weight:bold;color:#1e2f43;text-decoration:none;cursor: pointer;"><img src="img/icons/ciudadesyseticonospng-01.png" width="25%"><br>Lazaro Cardenas</span><br>
-
-<span style="font-size:10pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="cargar('#contenido2', '2.2.php')" ><img src="img/icons/ciudadesyseticonospng-05.png" width="25%"><br>Tapachula</span><br>
+<span style="font-size:11pt;font-weight:bold;color:#1e2f43;text-decoration:none;cursor: pointer;"> Gobernanza</span><br>
 
 
 
@@ -17,10 +15,7 @@
                         <div  style="text-align:justify;">
                         <ul style="list-style:none;cursor: pointer;color: #1e2f43;font-family:Montserrat;font-size:13.3px;
                         font weight:999;">
-                        <li onclick="ejecuta_ajax('2.2.1.php','','contenido1.2')" style="color:#e3313d;">La ciudad</li>
-                        <li onclick="ejecuta_ajax('2.2.2.php','','contenido1.2')" >Programa   municipal</li>
-                        <li href="#about" onclick="ejecuta_ajax('2.2.3.php','','contenido1.2')" >Indicadores</li>
-                        <li onclick="ejecuta_ajax('2.2.4.php','','contenido1.2')" >Ambito Espacial</li>
+                <li onclick="ejecuta_ajax('4.1.1.php','','contenido1.4')" style="color:#1e2f43;">¿Por qué un mecanismo de gobernanza?</li>       
 
                         </ul>   
                         </div>
@@ -30,7 +25,7 @@
                         <div class="col-md-4">
 
                         <div id="global">
-                        <div id="contenido1.2">
+                        <div id="contenido1.4">
 
                         </div>
                         </div>

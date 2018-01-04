59963bd02dbf6635e3bab5e8dd73c46807a48ba0
carlos michaus <c.michaus@incapital.mx>
2018-01-04 14:14:14 -0600

    bold
    bold de 1

--- index.php
+++ index.php
@@ -222,7 +222,7 @@
 
                <script>
                  function activass1(){
-                   document.getElementById("ss1.1").style="font-weight:bold;cursor: pointer;color: #1e2f43;font-family:Montserrat;font-size:9.5pt"
+                   document.getElementById("ss1.1").style="font-weight:bold;cursor: pointer;color: #1e2f43;font-family:Montserrat;font-size:10pt"
                    document.getElementById("ss1.2").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.3").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.4").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
@@ -230,7 +230,7 @@
                  }
 
                  function activass2(){
-                   document.getElementById("ss1.2").style="font-weight:bold;font-size:9.5pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
+                   document.getElementById("ss1.2").style="font-weight:bold;font-size:10pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.1").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.3").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.4").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
@@ -239,7 +239,7 @@
                  }
 
                  function activass3(){
-                   document.getElementById("ss1.3").style="font-weight:bold;font-size:9.5pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
+                   document.getElementById("ss1.3").style="font-weight:bold;font-size:10pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.1").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.2").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.4").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
@@ -248,7 +248,7 @@
                  }
 
                  function activass4(){
-                   document.getElementById("ss1.4").style="font-weight:bold;font-size:9.5pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
+                   document.getElementById("ss1.4").style="font-weight:bold;font-size:10pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.1").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.2").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.3").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
@@ -257,7 +257,7 @@
                  }
 
                  function activass5(){
-                   document.getElementById("ss1.5").style="font-weight:bold;font-size:9.5pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
+                   document.getElementById("ss1.5").style="font-weight:bold;font-size:10pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.1").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.2").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
                    document.getElementById("ss1.3").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"
@@ -266,7 +266,7 @@
                  }
 
                  function activass6(){
-                   document.getElementById("ss1.6").style="font-weight:bold;font-size:9.5pt;color:#1e2f43;text-decoration:none;cursor: pointer"
+                   document.getElementById("ss1.6").style="font-weight:bold;font-size:10pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("ss1.1").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("ss1.2").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("ss1.3").style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer"
@@ -275,26 +275,26 @@
                  }
 
                  function activasss1(){
-                   document.getElementById("sss1.1").style="font-weight:bold;font-size:8.5pt;color:#1e2f43;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.1").style="font-weight:bold;font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.2").style="font-size:8pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.3").style="font-size:8pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                  }
 
                  function activasss2(){
-                   document.getElementById("sss1.2").style="font-weight:bold;font-size:8.5pt;color:#1e2f43;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.2").style="font-weight:bold;font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.1").style="font-size:8pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.3").style="font-size:8pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                  }
 
                  function activasss3(){
-                   document.getElementById("sss1.3").style="font-weight:bold;font-size:8.5pt;color:#1e2f43;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.3").style="font-weight:bold;font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.1").style="font-size:8pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.2").style="font-size:8pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                  }
 
 
                  function activasss4(){
-                   document.getElementById("sss1.7").style="font-weight:bold;font-size:8.5pt;color:#e3313d;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.7").style="font-weight:bold;font-size:9pt;color:#e3313d;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.8").style="font-size:8pt;color:#19481e;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.9").style="font-size:8pt;color:#f0b65a;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.10").style="font-size:8pt;color:#e37435;text-decoration:none;cursor: pointer"
@@ -307,7 +307,7 @@
 
                  function activasss5(){
                    document.getElementById("sss1.7").style="font-size:8pt;color:#e3313d;text-decoration:none;cursor: pointer"
-                   document.getElementById("sss1.8").style="font-weight:bold;font-size:8.5pt;color:#19481e;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.8").style="font-weight:bold;font-size:9pt;color:#19481e;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.9").style="font-size:8pt;color:#f0b65a;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.10").style="font-size:8pt;color:#e37435;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.11").style="font-size:8pt;color:#7b223c;text-decoration:none;cursor: pointer"
@@ -320,7 +320,7 @@
                  function activasss6(){
                    document.getElementById("sss1.7").style="font-size:8pt;color:#e3313d;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.8").style="font-size:8pt;color:#19481e;text-decoration:none;cursor: pointer"
-                   document.getElementById("sss1.9").style="font-weight:bold;font-size:8.5pt;color:#f0b65a;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.9").style="font-weight:bold;font-size:9pt;color:#f0b65a;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.10").style="font-size:8pt;color:#e37435;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.11").style="font-size:8pt;color:#7b223c;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.12").style="font-size:8pt;color:#9bccd2;text-decoration:none;cursor: pointer"
@@ -333,7 +333,7 @@
                    document.getElementById("sss1.7").style="font-size:8pt;color:#e3313d;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.8").style="font-size:8pt;color:#19481e;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.9").style="font-size:8pt;color:#f0b65a;text-decoration:none;cursor: pointer"
-                   document.getElementById("sss1.10").style="font-weight:bold;font-size:8.5pt;color:#e37435;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.10").style="font-weight:bold;font-size:9pt;color:#e37435;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.11").style="font-size:8pt;color:#7b223c;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.12").style="font-size:8pt;color:#9bccd2;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.13").style="font-size:8pt;color:#485e6a;text-decoration:none;cursor: pointer"
@@ -346,7 +346,7 @@
                    document.getElementById("sss1.8").style="font-size:8pt;color:#19481e;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.9").style="font-size:8pt;color:#f0b65a;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.10").style="font-size:8pt;color:#e37435;text-decoration:none;cursor: pointer"
-                   document.getElementById("sss1.11").style="font-weight:bold;font-size:8.5pt;color:#7b223c;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.11").style="font-weight:bold;font-size:9pt;color:#7b223c;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.12").style="font-size:8pt;color:#9bccd2;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.13").style="font-size:8pt;color:#485e6a;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.14").style="font-size:8pt;color:#1e2f43;text-decoration:none;cursor: pointer"
@@ -359,7 +359,7 @@
                    document.getElementById("sss1.9").style="font-size:8pt;color:#f0b65a;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.10").style="font-size:8pt;color:#e37435;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.11").style="font-size:8pt;color:#7b223c;text-decoration:none;cursor: pointer"
-                   document.getElementById("sss1.12").style="font-weight:bold;font-size:8.5pt;color:#9bccd2;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.12").style="font-weight:bold;font-size:9pt;color:#9bccd2;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.13").style="font-size:8pt;color:#485e6a;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.14").style="font-size:8pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.15").style="font-size:8pt;color:#403c3b;text-decoration:none;cursor: pointer"
@@ -372,7 +372,7 @@
                    document.getElementById("sss1.10").style="font-size:8pt;color:#e37435;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.11").style="font-size:8pt;color:#7b223c;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.12").style="font-size:8pt;color:#9bccd2;text-decoration:none;cursor: pointer"
-                   document.getElementById("sss1.13").style="font-weight:bold;font-size:8.5pt;color:#485e6a;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.13").style="font-weight:bold;font-size:9pt;color:#485e6a;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.14").style="font-size:8pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.15").style="font-size:8pt;color:#403c3b;text-decoration:none;cursor: pointer"
                  }
@@ -385,7 +385,7 @@
                    document.getElementById("sss1.11").style="font-size:8pt;color:#7b223c;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.12").style="font-size:8pt;color:#9bccd2;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.13").style="font-size:8pt;color:#485e6a;text-decoration:none;cursor: pointer"
-                   document.getElementById("sss1.14").style="font-weight:bold;font-size:8.5pt;color:#1e2f43;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.14").style="font-weight:bold;font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.15").style="font-size:8pt;color:#403c3b;text-decoration:none;cursor: pointer"
                  }
 
@@ -398,7 +398,7 @@
                    document.getElementById("sss1.12").style="font-size:8pt;color:#9bccd2;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.13").style="font-size:8pt;color:#485e6a;text-decoration:none;cursor: pointer"
                    document.getElementById("sss1.14").style="font-size:8pt;color:#1e2f43;text-decoration:none;cursor: pointer"
-                   document.getElementById("sss1.15").style="font-weight:bold;font-size:8.5pt;color:#403c3b;text-decoration:none;cursor: pointer"
+                   document.getElementById("sss1.15").style="font-weight:bold;font-size:9pt;color:#403c3b;text-decoration:none;cursor: pointer"
                  }
 
 

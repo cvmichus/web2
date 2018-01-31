1fad9247e021863b45bccfd8bdc1fde03a393ef1
Carlos Valentin Michaus Barcenas <33094136+cvmichus@users.noreply.github.com>
2018-01-31 16:46:09 -0600

    gloss

--- 7.0.1.php
+++ 7.0.1.php
@@ -1,4 +1,4 @@
-<span id="t711" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.1.php','','contenido7.7');t711();limpia7();">7.1 Administrador Integral</span><br><br>
+<span id="t711" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.1.php','','contenido7.7');t711();limpia7();">Administrador Integral</span><br><br>
 
 					<span id="t712" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.2.php','','contenido7.7');t712();" >Autoridad Federal para el Desarrollo de las Zonas Económicas Especiales</span><br><br>
 
@@ -7,7 +7,36 @@
 					<span id="t714" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.4.php','','contenido7.7');t714();" >Comisión Especial de Zonas Económicas Especiales</span><br><br>
 					
 					<span id="t715" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.5.php','','contenido7.7');t715()" >Comisión Intersecretarial de Zonas Económicas Especiales</span><br><br>
-
+					
+					<span id="t716" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.6.php','','contenido7.7');t715()" >Comisión Interinstitucional Estatal</span><br><br>
+					
+					<span id="t717" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.7.php','','contenido7.7');t715()" >Consejo Local de Ordenamiento Territorial y Desarrollo Urbano</span><br><br>
+					
+					<span id="t718" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.8.php','','contenido7.7');t715()" >Consejo Municipal de Ordenamiento Territorial y Desarrollo Urbano</span><br><br>
+					
+					<span id="t719" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.9.php','','contenido7.7');t715()" >Consejo Nacional de Ordenamiento Territorial y Desarrollo Urbano</span><br><br>
+					
+					<span id="t720" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.10.php','','contenido7.7');t715()" >Consejo Técnico de la Zona / Consejo Técnico Multidisciplinario</span><br><br>
+					
+					<span id="t721" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.11.php','','contenido7.7');t715()" >Inversionistas</span><br><br>
+					
+					<span id="t722" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.12.php','','contenido7.7');t715()" >Secetaría de Desarrollo Agrario,Territorial y Urbano</span><br><br>
+					
+					<span id="t723" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.13.php','','contenido7.7');t715()" >Secetaría de Hacienda y Crédito Público</span><br><br>
+					
+					<span id="t724" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.14.php','','contenido7.7');t715()" >Ley Federal de Zonas Económicas Especiales</span><br><br>
+					
+					<span id="t725" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.15.php','','contenido7.7');t715()" >Reglamento de la Ley Federal de Zonas Económicas Especiales</span><br><br>
+					
+					<span id="t726" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.16.php','','contenido7.7');t715()" >Ley General de Asentamientos Humanos, Desarrollo Urbano y Ordenamiento Territorial</span><br><br>
+					
+					<span id="t727" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.17.php','','contenido7.7');t715()" >Actividades Económicas Productivas</span><br><br>
+					
+					<span id="t728" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.18.php','','contenido7.7');t715()" >Área de Influencia</span><br><br>
+					
+					<span id="t729" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.19.php','','contenido7.7');t715()" >Área Urbanizable</span><br><br>
+					
+					<span id="t730" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.20.php','','contenido7.7');t715()" > </span><br><br>
 
 
 

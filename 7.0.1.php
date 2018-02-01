a378591be6154b5218d7b75dad31620d0c002057
cvmichus <c.michaus@incapital.mx>
2018-02-01 00:36:44 -0600

    titulos glosario
    se terminaron los tituloa del glosario

--- 7.0.1.php
+++ 7.0.1.php
@@ -8,35 +8,134 @@
 					
 					<span id="t715" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.5.php','','contenido7.7');t715()" >Comisión Intersecretarial de Zonas Económicas Especiales</span><br><br>
 					
-					<span id="t716" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.6.php','','contenido7.7');t715()" >Comisión Interinstitucional Estatal</span><br><br>
+					<span id="t716" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.6.php','','contenido7.7');t716()" >Comisión Interinstitucional Estatal</span><br><br>
 					
-					<span id="t717" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.7.php','','contenido7.7');t715()" >Consejo Local de Ordenamiento Territorial y Desarrollo Urbano</span><br><br>
+					<span id="t717" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.7.php','','contenido7.7');t717()" >Consejo Local de Ordenamiento Territorial y Desarrollo Urbano</span><br><br>
 					
-					<span id="t718" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.8.php','','contenido7.7');t715()" >Consejo Municipal de Ordenamiento Territorial y Desarrollo Urbano</span><br><br>
+					<span id="t718" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.8.php','','contenido7.7');t718()" >Consejo Municipal de Ordenamiento Territorial y Desarrollo Urbano</span><br><br>
 					
-					<span id="t719" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.9.php','','contenido7.7');t715()" >Consejo Nacional de Ordenamiento Territorial y Desarrollo Urbano</span><br><br>
+					<span id="t719" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.9.php','','contenido7.7');t719()" >Consejo Nacional de Ordenamiento Territorial y Desarrollo Urbano</span><br><br>
 					
-					<span id="t720" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.10.php','','contenido7.7');t715()" >Consejo Técnico de la Zona / Consejo Técnico Multidisciplinario</span><br><br>
+					<span id="t720" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.10.php','','contenido7.7');t7110()" >Consejo Técnico de la Zona / Consejo Técnico Multidisciplinario</span><br><br>
 					
-					<span id="t721" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.11.php','','contenido7.7');t715()" >Inversionistas</span><br><br>
+					<span id="t721" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.11.php','','contenido7.7');t7111()" >Inversionistas</span><br><br>
 					
-					<span id="t722" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.12.php','','contenido7.7');t715()" >Secetaría de Desarrollo Agrario,Territorial y Urbano</span><br><br>
+					<span id="t722" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.12.php','','contenido7.7');t7112()" >Secetaría de Desarrollo Agrario,Territorial y Urbano</span><br><br>
 					
-					<span id="t723" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.13.php','','contenido7.7');t715()" >Secetaría de Hacienda y Crédito Público</span><br><br>
+					<span id="t723" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.13.php','','contenido7.7');t7113()" >Secetaría de Hacienda y Crédito Público</span><br><br>
 					
-					<span id="t724" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.14.php','','contenido7.7');t715()" >Ley Federal de Zonas Económicas Especiales</span><br><br>
+					<span id="t724" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.14.php','','contenido7.7');t7114()" >Ley Federal de Zonas Económicas Especiales</span><br><br>
 					
-					<span id="t725" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.15.php','','contenido7.7');t715()" >Reglamento de la Ley Federal de Zonas Económicas Especiales</span><br><br>
+					<span id="t725" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.15.php','','contenido7.7');t7115()" >Reglamento de la Ley Federal de Zonas Económicas Especiales</span><br><br>
 					
-					<span id="t726" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.16.php','','contenido7.7');t715()" >Ley General de Asentamientos Humanos, Desarrollo Urbano y Ordenamiento Territorial</span><br><br>
+					<span id="t726" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.16.php','','contenido7.7');t7116()" >Ley General de Asentamientos Humanos, Desarrollo Urbano y Ordenamiento Territorial</span><br><br>
 					
-					<span id="t727" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.17.php','','contenido7.7');t715()" >Actividades Económicas Productivas</span><br><br>
+					<span id="t727" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.17.php','','contenido7.7');t7117()" >Actividades Económicas Productivas</span><br><br>
 					
-					<span id="t728" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.18.php','','contenido7.7');t715()" >Área de Influencia</span><br><br>
+					<span id="t728" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.18.php','','contenido7.7');t7118()" >Área de Influencia</span><br><br>
 					
-					<span id="t729" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.19.php','','contenido7.7');t715()" >Área Urbanizable</span><br><br>
+					<span id="t729" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.19.php','','contenido7.7');t7119()" >Área Urbanizable</span><br><br>
 					
-					<span id="t730" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.20.php','','contenido7.7');t715()" > </span><br><br>
+					<span id="t730" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.20.php','','contenido7.7');t7120()" >Área Urbanizada </span><br><br>
+					
+					<span id="t731" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.21.php','','contenido7.7');t7121()" >Asentamiento Humano</span><br><br>
+					
+					<span id="t732" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.22.php','','contenido7.7');t7122 >Asignación</span><br><br>
+					
+					<span id="t733" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.23.php','','contenido7.7');t7123()" >Autorización</span><br><br>
+					
+					<span id="t734" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.24.php','','contenido7.7');t7124()" >Barrio</span><br><br>
+					
+					<span id="t735" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.25.php','','contenido7.7');t7125()" >Carta de Intención</span><br><br>
+					
+					<span id="t736" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.26.php','','contenido7.7');t7126()" >Centros de población </span><br><br>
+					
+					<span id="t737" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.27.php','','contenido7.7');t7127()" >Convenio de Coordinación</span><br><br>
+					
+					<span id="t738" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.28.php','','contenido7.7');t7128()" >Conurbación</span><br><br>
+					
+					<span id="t739" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.29.php','','contenido7.7');t7129()" >Espacio Público</span><br><br>
+					
+					<span id="t740" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.30.php','','contenido7.7');t7130()" >Espacio Edificable</span><br><br>
+					
+					<span id="t741" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.31.php','','contenido7.7');t7131()" >Estrategia Nacional de Ordenamiento Territorial</span><br><br>
+					
+					<span id="t742" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.32.php','','contenido7.7');t7132()" >Estudio de Prefactibilidad</span><br><br>
+					
+					<span id="t743" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.33.php','','contenido7.7');t7133()" >Evaluación Estratégica</span><br><br>
+					
+					<span id="t744" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.34.php','','contenido7.7');t7134()" >Densificación</span><br><br>
+					
+					<span id="t745" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.35.php','','contenido7.7');t7135()" >Desarrollo Urbano</span><br><br>
+					
+					<span id="t746" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.36.php','','contenido7.7');t7136()" >Desarrollo Metropolitano</span><br><br>
+					
+					<span id="t747" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.37.php','','contenido7.7');t7137()" >Dictamen de la ZEE</span><br><br>
+					
+					<span id="t748" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.38.php','','contenido7.7');t7138()" >Gestión Integral de RIesgos</span><br><br>
+					
+					<span id="t749" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.39.php','','contenido7.7');t7139()" >Infraestructura</span><br><br>
+					
+					<span id="t750" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.40.php','','contenido7.7');t7140()" >Movilidad</span><br><br>
+					
+					<span id="t751" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.41.php','','contenido7.7');t7141()" >Ordenamiento Ecológico General</span><br><br>
+					
+					<span id="t752" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.42.php','','contenido7.7');t7142()" >Ordenamiento Ecológico Regional o Municipal</span><br><br>
+					
+					<span id="t753" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.43.php','','contenido7.7');t7143()" >Ordenamiento Territorial de los Asentamientos Humanos</span><br><br>
+					
+					<span id="t754" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.44.php','','contenido7.7');t7144()" >Permiso</span><br><br>
+					
+					<span id="t755" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.45.php','','contenido7.7');t7145()" >Plan Estatal de Desarrollo</span><br><br>
+					
+					<span id="t756" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.46.php','','contenido7.7');t7146()" >Plan Estatal de Desarrollo Urbano</span><br><br>
+					
+					<span id="t757" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.47.php','','contenido7.7');t7147()" >Plan Maestro de la Zona</span><br><br>
+					
+					<span id="t758" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.48.php','','contenido7.7');t7148()" >Plan Municipal de Desarrollo</span><br><br>
+					
+					<span id="t759" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.49.php','','contenido7.7');t7149()" >Plan Nacional de Desarrollo</span><br><br>
+					
+					<span id="t760" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.50.php','','contenido7.7');t7150()" >Programa de Desarrollo Urbano de Centro de Población</span><br><br>
+					
+					<span id="t761" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.51.php','','contenido7.7');t7151()" >Programa de Desarrollo para ZEE</span><br><br>
+					
+					<span id="t762" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.52.php','','contenido7.7');t7152()" >Programa Municipal de Desarrollo Urbano</span><br><br>
+					
+					<span id="t763" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.53.php','','contenido7.7');t7153()" >Programa Nacional de Ordenmiento Territorial y Desarrollo Urbano</span><br><br>
+					
+					<span id="t764" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.54.php','','contenido7.7');t7154()" >Programa Metropolitano o de Zonas Conurbadas</span><br><br>
+					
+					<span id="t765" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.55.php','','contenido7.7');t7155()" >Programas Parciales</span><br><br>
+					
+					<span id="t766" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.56.php','','contenido7.7');t7156()" >Reglas de operación de la Zona</span><br><br>
+					
+					<span id="t767" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.57.php','','contenido7.7');t7157()" >Reservas Territoriales</span><br><br>
+					
+					<span id="t768" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.58.php','','contenido7.7');t7158()" >Servicios Asociados</span><br><br>
+					
+					<span id="t769" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.59.php','','contenido7.7');t7159()" >Servicios Urbanos</span><br><br>
+					
+					<span id="t770" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.60.php','','contenido7.7');t7160()" >Sistema Nacional de Planeación</span><br><br>
+					
+					<span id="t771" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.61.php','','contenido7.7');t7161()" >Sistema Nacional Territorial</span><br><br>
+					
+					<span id="t772" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.62.php','','contenido7.7');t7162()" >Sistemas Urbano Rurales</span><br><br>
+					
+					<span id="t773" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.63.php','','contenido7.7');t7163()" >Usos de Suelo</span><br><br>
+					
+					<span id="t774" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.64.php','','contenido7.7');t7164()" >Zona Económica Especial</span><br><br>
+					
+					<span id="t775" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.65.php','','contenido7.7');t7165()" >Zona Metropolitana</span><br><br>
+					
+					<span id="t776" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.66.php','','contenido7.7');t7166()" >Zonificación</span><br><br>
+					
+					<span id="t777" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.67.php','','contenido7.7');t7167()" >Zonificación primaria</span><br><br>
+					
+					<span id="t778" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;"  onclick="ejecuta_ajax('7.1.68.php','','contenido7.7');t7168()" >Zonificación secundiaria</span><br><br>
+					
+					
+					
 
 
 

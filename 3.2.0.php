62afbfe4a4ceacc8c2ff7b4488b02f011ebf3235
cvmichus <c.michaus@incapital.mx>
2018-04-23 23:34:00 -0500

    3.1.0 y 3.2.0
    cambios

--- 3.2.0.php
+++ 3.2.0.php
@@ -6,12 +6,13 @@
 
 <span id="sss324" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;" onclick="ejecuta_ajax('3.2.4.php','','contenido3.3');activasec324();">3.2.4 Estudios técnicos existentes</span><br><br>
 
+<!--
 <span id="sss325" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;" onclick="ejecuta_ajax('3.2.5.php','','contenido3.3');activasec325();">3.2.5 Marco legal actual</span><br><br>
 
 <span id="sss326" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;" onclick="ejecuta_ajax('3.2.6.php','','contenido3.3');activasec326();">3.2.6 Plan de Acción</span><br><br>
 
 <span id="sss327" style="font-size:9pt;color:#1e2f43;text-decoration:none;cursor: pointer;" onclick="ejecuta_ajax('3.2.7.php','','contenido3.3');activasec327();">3.2.7 Estatus de insumos para los Planes</span><br><br>
 
-
+-->
 
 
